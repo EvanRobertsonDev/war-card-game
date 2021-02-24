29dabfac7f22f287dc9144e3087943d4940ecf18
Evan Robertson <evan-robertson@outlook.com>
2021-02-23 20:11:53 -0500

    Commit base code

---
+++ src/cardgame/War.java
@@ -0,0 +1,21 @@
+/*
+ * Main
+ * 
+ * 
+ */
+package cardgame;
+
+/**
+ *
+ * @author Evan Robertson
+ */
+public class War {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
