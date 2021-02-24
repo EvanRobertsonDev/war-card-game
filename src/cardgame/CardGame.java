5cd73b3e4a9989ca902351094ac446bb63a3e91e
Evan Robertson <evan-robertson@outlook.com>
2021-02-23 19:52:49 -0500

    Commit base code

---
+++ src/cardgame/CardGame.java
@@ -0,0 +1,21 @@
+/*
+ * Main
+ * 
+ * 
+ */
+package cardgame;
+
+/**
+ *
+ * @author Evan Robertson
+ */
+public class CardGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
